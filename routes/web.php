86ff7eb0687c6212f4c977b015feb445af32a1c2
Vaibhav Pandey <contact@vaibhavpandey.com>
2022-10-28 22:16:42 +0530

    Enable registration by default.

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'home')->name('home');
 
-Auth::routes(['register' => false]);
+Auth::routes();
 
 Route::middleware('auth')->group(function () {
 
